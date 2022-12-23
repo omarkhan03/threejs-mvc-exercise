e5dfb3372172ba88bc101edb06691147118c71ca
omarkhan03 <omarahmadkhan2003@gmail.com>
2022-12-23 10:57:33 -0700

    Initial commit

---
+++ threejs-exercise/threejs-exercise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace threejs_exercise.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
